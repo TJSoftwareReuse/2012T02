91f4fc236af1a68d681a1fa0363eb543a8cc7146
孙琳 <2808411862@qq.com>
2015-05-30 19:14:01 +0800

    FM2.4

---
+++ FM/FM2.4/src/myPackage/main.java
@@ -0,0 +1,12 @@
+package myPackage;
+
+import com.manager.failure.*;
+
+public class main {
+	
+	public static void main(String[] args){
+	FailureManager myFM = new FailureManager();
+	boolean ttt = myFM.resetOutputFile("D:/myhahahah");
+	}
+
+}
