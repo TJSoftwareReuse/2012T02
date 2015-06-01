47700fd1f0fbf195d97706085e7e9fc25e67f64f
wangxiaoying <wangxiaoying0369@gmail.com>
2015-06-01 00:42:52 -0700

    FM3.0 update

--- FM/README.md
+++ FM/README.md
@@ -2,3 +2,71 @@
 
 1. 接收应用程序的告警信息
 2. 输出告警信息到一个单独的告警文件
+
+##FM2.0更新要求
+下一条警告切换到新文件
+
+###实现
+<code>
+log4j.rootLogger = DEBUG, FILE
+</code>
+
+<code>
+log4j.appender.FILE=org.apache.log4j.DailyRollingFileAppender
+</code>
+
+<code>
+log4j.appender.FILE.File=${WORKDIR}/logs/app.log
+</code>
+
+<code>
+log4j.appender.file.datePattern=’.'yyyy-MM-dd-HH-mm-SS
+</code>
+
+<code>
+log4j.appender.FILE.layout=org.apache.log4j.PatternLayout
+</code>
+
+<code>
+log4j.appender.FILE.layout.conversionPattern=%d{YYYY-MM-DD HH:mm:ss} %p %m%n
+</code>
+
+用log4j.appender.FILE=org.apache.log4j.DailyRollingFileAppender，以及log4j.appender.file.datePattern=’.'yyyy-MM-dd-HH-mm-SS实现每秒检查一次
+
+从而实现下一条警告切换到新文件，这个方法很无赖。。。
+
+##About FM2.1
+
+FM2.0里有些乱七八糟的代码，修复了一下
+然后FM2.0似乎是用不了的，
+
+FM2.1可用
+
+
+改了log文件的输出地址，然后把propertie文件放在了根目录下
+
+## FM3.0 更新日志
+
+###### 注：FM3.0代码部分有较大的改动
+
+### 支持
+
+* 所有接口的参数和返回值没有变动，对旧版本兼容
+* 依旧支持5中警告类型
+
+### 变动
+
+* 利用Log4j进行日志输出（而非告警输出），文件名为fm.log
+* 接口：```resetOutputFile```的意义有所变动，详细信息见下条
+* 警告输出到 ```FILE_PATH/yyyy-MM-dd HH:mm:ss-x.log``` 文件中
+	* ```FILE_PATH``` 为输出路径，由 ```resetOutputFile(String filename)``` 接口进行设置
+	* 文件名为```时间戳-ID.log```
+	* 每条警告信息输出到一个新文件中
+	* 连续相同的警告信息不输出
+	* 信息输出格式依旧为```警告类型 警告信息``` -- 文件名中已有时间戳
+
+
+### 依赖
+
+* FM3.0支持```jdk-1.6```及以上版本
+* FM3.0支持```log4j-1.2.17```及以上版本
---
+++ FM/src/com/manager/failure/FailureManager.java
@@ -0,0 +1,83 @@
+package com.manager.failure;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.log4j.Logger;
+
+
+public class FailureManager {
+	
+	private static Logger logger = Logger.getLogger(FailureManager.class.getName());
+	private static int counter = 0;
+	private static String fakenow = "00-00-00_00:00:00";
+	private static String filepath = ".";
+	
+	
+	public static void logInfo(String log){
+		log(log, "INFO");
+    }
+
+    public static void logDebug(String log){
+    	log(log, "DEBUG");
+    }
+
+    public static void logWarn(String log){
+    	log(log, "WARN");
+    }
+
+    public static void logError(String log){
+    	log(log, "ERROR");
+    }
+
+    public static void logFatal(String log){
+    	log(log, "FATAL");
+    }
+    
+    private static void log(String log, String level){
+    	String filename = generateUniqueName();
+    	log = level+" "+log;
+    	writeToFile(log, filename);
+    }
+
+    public synchronized static boolean resetOutputFile(String filepath){
+    	try{
+    		File dir = new File(filepath);
+    		dir.mkdirs();
+        	FailureManager.filepath = filepath;
+        	logger.info("Done: reset log directory to "+filepath);
+        	return true;
+    	}catch(Exception e){
+    		e.printStackTrace();
+    		logger.error("Failed reset log directory to "+filepath);
+    		return false;
+    	}
+    }
+    
+    private synchronized static String generateUniqueName(){
+    	String filename = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+		if(!FailureManager.fakenow.equals(filename)){
+			FailureManager.counter = 0;
+			FailureManager.fakenow = filename;
+		}
+		filename += "-"+Integer.toString(FailureManager.counter++)+".log";
+		return filename;
+    }
+    
+    private synchronized static void writeToFile(String log, String filename){
+		try {
+			PrintWriter printwriter = new PrintWriter(new File(FailureManager.filepath, filename));
+			printwriter.println(log);
+			printwriter.close();
+			logger.info("Done: write to file "+FailureManager.filepath+"/"+filename);
+//			logger.debug("Log: "+log);
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error("Failed: write to file "+filename);
+		}
+    }
+    
+    
+}
